9a53fb01f0106650b5c5c9ce236e16290f36256b
Deepeshgiri <dr.deepeshgiri@gmail.com>
2025-01-28 11:31:30 +0530

    cors

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const route  = require('./routes/allRoutes');
 const morgan = require('morgan');
 const cors = require('cors');
+const route = require('./routes/allRoutes');
 const User = require('./models/userModel');
 
+// Load environment variables
 dotenv.config();
 
 // Initialize Express app
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(morgan('dev'));
-app.use(cors());
-app.options('*', cors());
-// Use routes
+
+// CORS Configuration
+const corsOptions = {
+  origin: 'https://referral-front-topaz.vercel.app', // Replace with your frontend URL
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+
+
+// Routes
 app.use('/', route);
 
-// const abc =async()=>{
-//   const getStage = await User.getAllStage();
-// console.log("getStage:", getStage);
-// }
+// Global Error Handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
 
-// abc()
-
-
-
-// Start server
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
